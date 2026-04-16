f2041203d62e0ca4762c83a992a62f1ec0489823
Smarajit Datta <smarajitdatta6@gmail.com>
2026-04-16 13:16:48 +0530

    compile.sh: Enable walt scheduler

--- compile.sh
+++ compile.sh
@@ -227,6 +227,7 @@ while true; do
                 -e CONFIG_CPU_FREQ_GOV_USERSPACE \
                 -e CONFIG_CPU_FREQ_GOV_ONDEMAND \
                 -e CONFIG_CPU_FREQ_GOV_CONSERVATIVE \
+                -e CONFIG_SCHED_WALT \
                 -e CONFIG_CPU_BOOST \
                 
             # Config modifications for localversion

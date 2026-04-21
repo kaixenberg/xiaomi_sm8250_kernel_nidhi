df120efe9e5e08170ba29368da786502055daf79
EmanuelCN <emanuelghub@gmail.com>
2026-04-21 19:22:22 +0000

    sched: cpufreq_schedutil: switch to precomputed triangular linear headroom
    [kvsnr113: squashed changes from quadratic to simplify history]
    
    Signed-off-by: EmanuelCN <emanuelghub@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -239,7 +239,7 @@ static inline unsigned long sugov_apply_dvfs_headroom(unsigned long util,
 {
 	unsigned long delta, headroom;
 	unsigned long capped_util = min(util, capacity);
-	unsigned long delta_t = (capacity * 200) >> 10;
+	unsigned long delta_t = (capacity * 220) >> 10;
 
 	delta = capacity - capped_util;
 

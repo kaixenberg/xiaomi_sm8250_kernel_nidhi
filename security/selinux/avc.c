5febe88f5e68054c9cf27576ce56f67b6b66d526
kvsnr113 <kvsnrprojkt113@gmail.com>
2026-04-23 10:37:44 +0700

    security/selinux: properly guard more CONFIG_AUDIT cases
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -137,6 +137,7 @@ static inline int avc_hash(u32 ssid, u32 tsid, u16 tclass)
 	return (ssid ^ (tsid << 2) ^ (tclass << 4)) & (AVC_CACHE_SLOTS - 1);
 }
 
+#ifdef CONFIG_AUDIT
 /**
  * avc_dump_av - Display an access vector in human-readable form.
  * @tclass: target security class
@@ -218,6 +219,7 @@ bypass_orig_flow:
 	BUG_ON(!tclass || tclass >= ARRAY_SIZE(secclass_map));
 	audit_log_format(ab, " tclass=%s", secclass_map[tclass - 1].name);
 }
+#endif
 
 /**
  * avc_init - Initialize the AVC.
@@ -507,14 +509,18 @@ static inline int avc_xperms_audit(struct selinux_state *state, u32 ssid,
 				   struct extended_perms_decision *xpd, u8 perm,
 				   int result, struct common_audit_data *ad)
 {
+#ifdef CONFIG_AUDIT
 	u32 audited, denied;
 
 	audited = avc_xperms_audit_required(requested, avd, xpd, perm, result,
 					    &denied);
 	if (likely(!audited))
 		return 0;
-	return slow_avc_audit(state, ssid, tsid, tclass, requested, audited,
-			      denied, result, ad);
+	return slow_avc_audit(state, ssid, tsid, tclass, requested,
+			audited, denied, result, ad);
+#else
+	return 0;
+#endif
 }
 
 static void avc_node_free(struct rcu_head *rhead)
@@ -740,6 +746,7 @@ found:
 	return node;
 }
 
+#ifdef CONFIG_AUDIT
 /**
  * avc_audit_pre_callback - SELinux specific information
  * will be called by generic audit code
@@ -802,6 +809,7 @@ noinline int slow_avc_audit(struct selinux_state *state, u32 ssid, u32 tsid,
 	common_lsm_audit(a, avc_audit_pre_callback, avc_audit_post_callback);
 	return 0;
 }
+#endif
 
 /**
  * avc_add_callback - Register a callback for security events.
--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -3597,6 +3597,7 @@ out:
 	return match;
 }
 
+#ifdef CONFIG_AUDIT
 static int (*aurule_callback)(void) = audit_update_lsm_rules;
 
 static int aurule_avc_callback(u32 event)
@@ -3619,6 +3620,7 @@ static int __init aurule_init(void)
 	return err;
 }
 __initcall(aurule_init);
+#endif
 
 #ifdef CONFIG_NETLABEL
 /**

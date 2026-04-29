ddee35202345728a74471a967b66bc93aefb9248
Smarajit Datta <smarajitdatta6@gmail.com>
2026-04-29 10:53:03 +0530

    kernel: fix uname spoofing to 5.10

--- kernel/sys.c
+++ kernel/sys.c
@@ -1254,6 +1254,19 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
+	if (current_uid().val == 0 &&
+	    (!strncmp(current->comm, "bpfloader", 9) ||
+	     !strncmp(current->comm, "netbpfload", 10) ||
+	     !strncmp(current->comm, "netd", 4))) {
+		strcpy(tmp.release, "5.10.248");
+		pr_info("fake uname: %s/%d release=%s\n", current->comm,
+			current->pid, tmp.release);
+	} else if (cur_uid >= 1000) {
+		strlcpy(tmp.release, "5.10.248", sizeof(tmp.release));
+	}
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+	susfs_spoof_uname(&tmp);
+#endif
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
